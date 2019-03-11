971cbcec6460572a3cd18fdb865ff81bc215aab1
kkrupp1 <kyle.krupp@gmail.com>
2019-03-11 16:51:58 -0400

    Dashboard and profile state

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -23,7 +23,7 @@ const TextFieldGroup = ({
         placeholder={placeholder}
         name={name}
         value={value}
-        onChange={this.onChange}
+        onChange={onChange}
         disabled={disabled}
       />
       {info && <small className="form-text text-muted">{info}</small>}
@@ -39,7 +39,12 @@ TextFieldGroup.propTypes = {
   info: PropTypes.string,
   error: PropTypes.string,
   type: PropTypes.string.isRequired,
-  onChange: PropTypes.string.isRequired
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.string
+};
+
+TextFieldGroup.defaultProps = {
+  type: "text"
 };
 
 export default TextFieldGroup;
