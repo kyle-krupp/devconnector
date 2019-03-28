8282a7e391973a5940dc1d8dac5a276285a4053d
kkrupp1 <kyle.krupp@gmail.com>
2019-03-28 16:26:19 -0400

    first text field group for profile creation complete

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
-import { userInfo } from "os";
 
 class Dashboard extends Component {
   componentDidMount() {
