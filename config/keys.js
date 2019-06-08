97948fa4ac52312a41701063a3e14a0c683272fb
Kyle Krupp <kyle.krupp@gmail.com>
2019-06-08 14:31:31 -0400

    posts and comments functional, prepping DB environment for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://superduperdevconnector:superduperdevconnector1@ds119523.mlab.com:19523/superduperdevconnect",
-  secretOrKey: "secret"
-};
+if (proces.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
