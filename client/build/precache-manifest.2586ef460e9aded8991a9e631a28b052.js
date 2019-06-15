e6f3c964e5ed58ed3b9e85aba14bb5cbccfda594
Kyle Krupp <kyle.krupp@gmail.com>
2019-06-15 23:11:04 +0200

    build

---
+++ client/build/precache-manifest.2586ef460e9aded8991a9e631a28b052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bc9ada393e55dbcc91c5334455ca7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cac8bf3650c626e730f4",
+    "url": "/static/css/main.ea1499ba.chunk.css"
+  },
+  {
+    "revision": "0550f732204f305fe3c2",
+    "url": "/static/js/2.59131f66.chunk.js"
+  },
+  {
+    "revision": "cac8bf3650c626e730f4",
+    "url": "/static/js/main.58ae7cec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
