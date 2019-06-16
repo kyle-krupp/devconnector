a96d5ce801d0fdf37c4bbe23ee84e5a35e8a35f2
Kyle Krupp <kyle.krupp@gmail.com>
2019-06-16 15:12:00 +0200

    update production config

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI:
+    "mongodb://heroku_p3ll1151:rc6g3mmk0to44umbb2haf5deeq@ds133557.mlab.com:33557/heroku_p3ll1151",
+  secretOrKey: "secret"
 };
