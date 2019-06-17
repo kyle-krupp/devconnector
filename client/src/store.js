3c3c8ab4da8c63ef7d67d9a29c08b8be4e17e0e4
Kyle Krupp <kyle.krupp@gmail.com>
2019-06-17 16:52:44 +0200

    change variable types in store.js

--- client/src/store.js
+++ client/src/store.js
@@ -2,9 +2,18 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initialState = {};
+var initialState = {};
 
-const middleware = [thunk];
+var middleware = [thunk];
+
+var store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 if (process.env.NODE_ENV === "production") {
   store = createStore(
