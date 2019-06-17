96eea8ef71df9d2e649c127c191dbfa4f72e7cf5
Kyle Krupp <kyle.krupp@gmail.com>
2019-06-17 16:29:48 +0200

    remove dev tools, create logic for production

--- client/src/store.js
+++ client/src/store.js
@@ -6,15 +6,6 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-
 if (process.env.NODE_ENV === "production") {
   store = createStore(
     rootReducer,
